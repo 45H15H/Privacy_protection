78911728b9eff71edcff2c8260681a23d715f19d
Ashish Singh <ashish1qwerty1@gmail.com>
2023-06-22 22:37:43 +0530

    Add widgets

--- app.py
+++ app.py
@@ -8,8 +8,17 @@ from PIL import Image
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
+# change it to h2
+st.markdown("<h2 style='text-align: center; color: red;'>Image Anonymizer</h2>", unsafe_allow_html=True)
+
+st.caption("Protecting Privacy")
+
+
 # take image input from user
-uploaded_file = st.file_uploader("Upload Image")
+uploaded_file = st.file_uploader("Upload Image", accept_multiple_files=False)
+
+# choose between blur, pixelate and black rectangle
+option = st.selectbox("Choose the type of filter", ("Blur", "Pixelate", "Black Rectangle"))
 
 # wait for the user to upload an image
 if uploaded_file is not None:
@@ -25,47 +34,26 @@ if uploaded_file is not None:
     # detect faces
     faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.05, minNeighbors=5, minSize=(40, 40))
 
-    # draw a rectangle around the face
-    for (x, y, w, h) in faces:
+    # if blur is selected
+    if option == "Blur":
         # draw a rectangle around the face
-        cv2.rectangle(opencv_image, (x, y), (x+w, y+h), (255, 255, 0), 2)
-        # fill the rectangle with black color arond the eyes
-        cv2.rectangle(opencv_image, (x+int(w/6), y+int(h/5)),
-                      (x+w-int(w/6), y+h-int(h/2)), (0, 0, 0), -1)
+        for (x, y, w, h) in faces:
+            # draw a rectangle around the face
+            cv2.rectangle(opencv_image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+            # blur the face
+            opencv_image[y:y+h, x:x+w] = cv2.blur(opencv_image[y:y+h, x:x+w], (23, 23))
+        
+    # if black rectangle is selected
+    elif option == "Black Rectangle":
+    # draw a rectangle around the face
+        for (x, y, w, h) in faces:
+            # draw a rectangle around the face
+            cv2.rectangle(opencv_image, (x, y), (x+w, y+h), (255, 255, 0), 2)
+            # fill the rectangle with black color arond the eyes
+            cv2.rectangle(opencv_image, (x+int(w/6), y+int(h/5)), (x+w-int(w/6), y+h-int(h/2)), (0, 0, 0), -1)
 
-    # display the output image
-    st.image(opencv_image, caption="Face Detection", use_column_width=True)
+    button = st.button("Generate")
 
-
-
-
-
-
-# if image is not None:
-#     image_ = str(image)
-#     img = cv2.imread(image_, 1) # read image in BGR format
-
-#     # resize the image
-#     img = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
-
-#     # convert image to grayscale
-#     gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-#     # detect faces
-#     faces = face_cascade.detectMultiScale(gray_img, scaleFactor=1.05, minNeighbors=5, minSize=(40, 40))
-
-#     # draw a rectangle around the face
-#     for (x, y, w, h) in faces:
-#         # draw a rectangle around the face
-#         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 0), 2)
-#         # fill the rectangle with black color arond the eyes
-#         cv2.rectangle(img, (x+int(w/6), y+int(h/5)),
-#                       (x+w-int(w/6), y+h-int(h/2)), (0, 0, 0), -1)
-
-# # display the output image
-# st.image(img, caption="Face Detection", use_column_width=True)
-
-# # give download option
-# cv2.imwrite("output.jpg", img)
-
-# st.download_button(label="Download", data="output.jpg", file_name="output.jpg")
+    if button == True:
+        # display the output image
+        st.image(opencv_image, caption="Face Detection", use_column_width=True)
