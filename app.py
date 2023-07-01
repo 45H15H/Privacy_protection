e89fe7ada1a6a4060694dc3c1e75ba67770caca7
Ashish Singh <ashish1qwerty1@gmail.com>
2023-07-01 19:02:29 +0530

    Increase blur intensity

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ if uploaded_file is not None:
             # draw a rectangle around the face
             cv2.rectangle(opencv_image, (x, y), (x+w, y+h), (255, 255, 0), 2)
             # blur the face
-            opencv_image[y:y+h, x:x+w] = cv2.blur(opencv_image[y:y+h, x:x+w], (31, 31))
+            opencv_image[y:y+h, x:x+w] = cv2.blur(opencv_image[y:y+h, x:x+w], (61, 61))
         
     # if black rectangle is selected
     elif option == "Black Rectangle":
